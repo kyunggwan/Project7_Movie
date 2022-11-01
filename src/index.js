b38963677b4b6034e1864619cbb163ea83ac4c98
4562sky <4562sky@naver.com>
2022-11-01 14:11:27 +0900

    무비

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom/BrowserRouter';
+import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 serviceWorkerRegistration.register(); 
 
